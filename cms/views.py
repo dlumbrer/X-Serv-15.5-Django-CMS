70d264baf880ce22e5ed83d6f9d2f629783bfcf8
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-03-18 19:28:01 +0100

    Implementación completa

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from models import Pages
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+def mostrar(request, recurso):
+    try:
+        fila = Pages.objects.get(name=recurso)
+        print fila.name
+        return HttpResponse(request.method + " " + str(fila.id) + " " + fila.name + " " + fila.page)
+    except Pages.DoesNotExist:
+        return HttpResponseNotFound("Page not found: " + recurso)
+
+#OJO, la base de datos solo tiene /pepe y /pepito
