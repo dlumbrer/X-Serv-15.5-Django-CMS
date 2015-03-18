b8492fe923c8d85bfde7f2725a2ba15381e1221c
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-03-18 19:52:57 +0100

    Ahora funciona para añadir paginas con PUT

--- cms/views.py
+++ cms/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from models import Pages
 from django.http import HttpResponse, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def mostrar(request, recurso):
-    try:
-        fila = Pages.objects.get(name=recurso)
-        print fila.name
-        return HttpResponse(request.method + " " + str(fila.id) + " " + fila.name + " " + fila.page)
-    except Pages.DoesNotExist:
-        return HttpResponseNotFound("Page not found: " + recurso)
+    if request.method == "GET":
+        #MUESTRO DE LA BD
+        try:
+            fila = Pages.objects.get(name=recurso)
+            print fila.name
+            return HttpResponse(request.method + " " + str(fila.id) + " " + fila.name + " " + fila.page)
+        except Pages.DoesNotExist:
+            return HttpResponseNotFound("Page not found: " + recurso)
+    elif request.method == "PUT":
+        #GUARDO EN LA BD
+        p = Pages(name=recurso, page=request.body)
+        p.save()
+        return HttpResponse("guardada pagina, haz un get para comprobar")
 
 #OJO, la base de datos solo tiene /pepe y /pepito
